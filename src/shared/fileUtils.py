35409f82c43a6e78eb780bd63bb75871b543d43e
Carlos <Carlosrams2@outlook.es>
2025-07-23 22:15:15 -0600

    logic to detect datasets, show the datasets in front, select dataset and redirect into dataset page

--- src/shared/fileUtils.py
+++ src/shared/fileUtils.py
@@ -13,3 +13,14 @@ def is_folder_not_empty(path):
     if not root.exists() or not root.is_dir():
         return False
     return any(root.iterdir())
+
+def get_data_folders():
+    data_dir = Path("Data")
+    if not data_dir.exists() or not data_dir.is_dir():
+        return []
+
+    folders = [
+        folder.name for folder in data_dir.iterdir()
+        if folder.is_dir() and folder.name != "_aux"
+    ]
+    return folders
